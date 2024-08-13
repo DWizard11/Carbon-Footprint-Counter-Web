2682e8c4e395e84ba71561acccc9cab9ac4819e3
Daniyal <66713977+DWizard11@users.noreply.github.com>
2024-08-13 06:29:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, redirect, url_for, request
+import csv
+from datetime import date
+
+
+app = Flask(__name__)
+tips = ["Switch off the lights when not in use"]
+today = date.today()
+
+def get_logs(): 
+    # file closes after with block 
+    with open('logs.csv', 'r') as file: 
+        logs = []
+        reader = csv.reader(file) 
+        # skip the header row 
+        next(reader) 
+        # return the rest of the rows 
+        for line in reader: 
+            date = line[0]
+            activity = line[1]
+            footprint = line[2]
+            notes = line[3]
+            log = [date, activity, footprint, notes]
+            logs.append(log)
+        return logs 
+
+@app.route('/', methods=["POST", "GET"])
+def index():
+    logs = get_logs()
+
+    
+    return render_template('index.html', today=today, tips=tips)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
+# General posisble changes: 
+# 1. Use random module for tips? 
